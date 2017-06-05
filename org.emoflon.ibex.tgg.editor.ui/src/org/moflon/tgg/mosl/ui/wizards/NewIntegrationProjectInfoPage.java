bf8783a6c176543cc0e56cf700a1c316a7c22c46
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-06-05 13:38:28 +0200

    Added current GUI projects

---
+++ org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationProjectInfoPage.java
@@ -0,0 +1,9 @@
+package org.moflon.tgg.mosl.ui.wizards;
+
+public class NewIntegrationProjectInfoPage extends AbstractMoflonProjectInfoPage {
+
+	public NewIntegrationProjectInfoPage() {
+		super("NewIntegrationProjectInfo", "New Integration Project",
+				"This wizard creates a new integration eMoflon project.");
+	}
+}
