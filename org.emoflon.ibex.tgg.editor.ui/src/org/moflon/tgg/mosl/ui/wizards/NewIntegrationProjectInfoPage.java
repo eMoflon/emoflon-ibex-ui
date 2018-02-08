498c639302539543f396a9969f6fa506683983ca
Patrick Robrecht <mail@patrick-robrecht.de>
2018-02-08 14:29:26 +0100

    Rename "Integration Project" to "TGG Project" in wizard as well

--- org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationProjectInfoPage.java
+++ org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationProjectInfoPage.java
@@ -3,7 +3,7 @@ package org.moflon.tgg.mosl.ui.wizards;
 public class NewIntegrationProjectInfoPage extends AbstractMoflonProjectInfoPage {
 
 	public NewIntegrationProjectInfoPage() {
-		super("NewIntegrationProjectInfo", "New Integration Project",
-				"This wizard creates a new integration eMoflon project.");
+		super("NewIntegrationProjectInfo", "New TGG Project",
+				"This wizard creates a new eMoflon TGG Project.");
 	}
 }
