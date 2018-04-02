2d6ffcfcbc4f6fb8661d78d131d14d1093aa9bde
Patrick Robrecht <mail@patrick-robrecht.de>
2018-04-02 18:13:28 +0200

    Refactoring of PlatformFragmentLinkOpener

--- org.emoflon.ibex.gt.editor.ui/src/org/emoflon/ibex/gt/editor/ui/visualization/PlatformFragmentLinkOpener.java
+++ org.emoflon.ibex.gt.editor.ui/src/org/emoflon/ibex/gt/editor/ui/visualization/PlatformFragmentLinkOpener.java
@@ -5,11 +5,13 @@ import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IGotoMarker;
 import org.eclipse.ui.part.FileEditorInput;
@@ -18,50 +20,52 @@ import org.eclipse.xtext.ui.editor.XtextEditor;
 
 import net.sourceforge.plantuml.eclipse.utils.ILinkOpener;
 import net.sourceforge.plantuml.eclipse.utils.LinkData;
-import net.sourceforge.plantuml.eclipse.utils.PlatformLinkOpener;
 
 /**
- * An {@link ILinkOpener} with support for opening the editor on the line of the
- * fragment in a platform link.
+ * Custom {@link ILinkOpener} for opening the editor on the line of the fragment
+ * in a platform link.
  */
-public class PlatformFragmentLinkOpener extends PlatformLinkOpener {
+public class PlatformFragmentLinkOpener implements ILinkOpener {
 
 	@Override
 	public int supportsLink(final LinkData link) {
 		URI uri = URI.createURI(link.href);
-		if (uri.fileExtension().equals("gt") && uri.hasFragment()) {
+		if (uri.fileExtension().equals("gt") && uri.isPlatformResource() && uri.hasFragment()) {
 			return CUSTOM_SUPPORT;
 		}
-		return super.supportsLink(link);
-	}
-
-	@Override
-	protected IPath getPath(final LinkData link) {
-		URI uri = URI.createURI(link.href);
-		if (!uri.hasFragment()) {
-			return super.getPath(link);
-		}
-		LinkData linkWithoutFragment = new LinkData();
-		linkWithoutFragment.href = uri.trimFragment().toString();
-		return super.getPath(linkWithoutFragment);
+		return NO_SUPPORT;
 	}
 
 	@Override
 	public void openLink(final LinkData link) {
 		URI uri = URI.createURI(link.href);
 
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(getPath(link));
-		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		IEditorDescriptor desc = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(file.getName());
+		IPath path = new Path(uri.path()).removeFirstSegments(1);
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
 
 		try {
-			IEditorPart editorPart = page.openEditor(new FileEditorInput(file), desc.getId());
+			IEditorPart editorPart = openEditor(file);
 			if (editorPart instanceof XtextEditor) {
 				goToFragmentLine((XtextEditor) editorPart, file, uri.fragment());
 			}
 		} catch (CoreException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * Opens the default editor for the given file and returns it.
+	 * 
+	 * @param file
+	 *            the file
+	 * @return the opened editor
+	 * @throws PartInitException
+	 *             if the editor could not be created or initialized
+	 */
+	private static IEditorPart openEditor(final IFile file) throws PartInitException {
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IEditorDescriptor desc = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(file.getName());
+		return page.openEditor(new FileEditorInput(file), desc.getId());
 	}
 
 	/**
