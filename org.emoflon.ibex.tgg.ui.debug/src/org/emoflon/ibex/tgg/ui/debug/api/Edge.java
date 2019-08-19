b890e2abcd1ee55330a4d1117540e85eec818aec
Mario Treiber <mtr@mail.upb.de>
2019-08-19 18:41:46 +0200

    Add BindingType and EdgeType to Edge

--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Edge.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Edge.java
@@ -1,5 +1,8 @@
 package org.emoflon.ibex.tgg.ui.debug.api;
 
+import org.emoflon.ibex.tgg.ui.debug.api.enums.EdgeType;
+import org.emoflon.ibex.tgg.ui.debug.api.enums.VictoryBindingType;
+
 public interface Edge {
 
     public String getLabel();
@@ -7,4 +10,8 @@ public interface Edge {
     public Node getSrcNode();
 
     public Node getTrgNode();
+
+    public EdgeType getType();
+
+    public VictoryBindingType getBindingType();
 }
--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/impl/EdgeImpl.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/impl/EdgeImpl.java
@@ -2,11 +2,15 @@ package org.emoflon.ibex.tgg.ui.debug.api.impl;
 
 import org.emoflon.ibex.tgg.ui.debug.api.Edge;
 import org.emoflon.ibex.tgg.ui.debug.api.Node;
+import org.emoflon.ibex.tgg.ui.debug.api.enums.EdgeType;
+import org.emoflon.ibex.tgg.ui.debug.api.enums.VictoryBindingType;
 
 public class EdgeImpl implements Edge {
     private String label;
     private Node srcNode;
     private Node trgNode;
+    private EdgeType type;
+    private VictoryBindingType bindingType;
 
     public EdgeImpl(String pLabel, Node pSrcNode, Node pTrgNode) {
 	label = pLabel;
@@ -37,4 +41,21 @@ public class EdgeImpl implements Edge {
     public void setTrgNode(Node pTrgNode) {
 	trgNode = pTrgNode;
     }
+
+    public EdgeType getType() {
+	return type;
+    }
+
+    public void setType(EdgeType pType) {
+	type = pType;
+    }
+
+    public VictoryBindingType getBindingType() {
+	return bindingType;
+    }
+
+    public void setBindingType(VictoryBindingType pBindingType) {
+	bindingType = pBindingType;
+    }
+
 }
