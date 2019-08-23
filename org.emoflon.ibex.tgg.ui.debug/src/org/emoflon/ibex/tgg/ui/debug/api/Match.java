9d1a88e4c7db53ee51e69afd2335e6ecb83d7771
Ayurshi <ayurshi.singh75@gmail.com>
2019-08-23 11:47:34 +0200

    Rule and Match APIs added

---
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Match.java
@@ -0,0 +1,9 @@
+package org.emoflon.ibex.tgg.ui.debug.api;
+
+import java.util.Collection;
+
+public interface Match {
+	String getRuleName() ;
+	Collection<String> getParameterNames() ;
+	String getPatternName() ;
+}
---
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Rule.java
@@ -0,0 +1,9 @@
+package org.emoflon.ibex.tgg.ui.debug.api;
+
+import java.util.List;
+
+
+public interface Rule {
+	List<Node> getNodes();
+	List<Edge> getEdges();
+}
