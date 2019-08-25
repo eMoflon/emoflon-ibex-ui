da4944eeb972925595b7d58976344994ef762786
Mario Treiber <mariotreiber@gmail.com>
2019-08-25 23:12:38 +0200

    Extend Graphs to contain collections of node-groupings

--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/impl/GraphImpl.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/impl/GraphImpl.java
@@ -8,15 +8,15 @@ import org.emoflon.ibex.tgg.ui.debug.api.Graph;
 import org.emoflon.ibex.tgg.ui.debug.api.Node;
 
 public class GraphImpl implements Graph {
-    private Collection<Node> nodes;
+    private Collection<Collection<Node>> nodes;
     private Collection<Edge> edges;
 
-    GraphImpl(Collection<Node> pNodes, Collection<Edge> pEdges) {
+    GraphImpl(Collection<Collection<Node>> pNodes, Collection<Edge> pEdges) {
 	nodes = Collections.unmodifiableCollection(pNodes);
 	edges = Collections.unmodifiableCollection(pEdges);
     }
 
-    public Collection<Node> getNodes() {
+    public Collection<Collection<Node>> getNodes() {
 	return nodes;
     }
 
