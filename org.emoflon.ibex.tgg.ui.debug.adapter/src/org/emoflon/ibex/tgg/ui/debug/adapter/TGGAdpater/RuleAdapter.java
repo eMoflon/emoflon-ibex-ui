d4d4d6d64b59d7e24ac9710002dc33ab1a9c57f1
Ayurshi <ayurshi.singh75@gmail.com>
2019-08-27 17:22:02 +0200

    nodes and edges added using graphBuilder in ruleAdapter

--- org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/RuleAdapter.java
+++ org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/RuleAdapter.java
@@ -1,10 +1,15 @@
 package org.emoflon.ibex.tgg.ui.debug.adapter.TGGAdpater;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.emoflon.ibex.tgg.ui.debug.api.Edge;
 import org.emoflon.ibex.tgg.ui.debug.api.Graph;
+import org.emoflon.ibex.tgg.ui.debug.api.Node;
 import org.emoflon.ibex.tgg.ui.debug.api.Rule;
+import org.emoflon.ibex.tgg.ui.debug.api.impl.GraphBuilder;
 
 import language.TGGRule;
 
@@ -23,9 +28,22 @@ public class RuleAdapter implements Rule {
     }
 
     private TGGRule rule;
+    private GraphBuilder graphBuilder;
 
     private RuleAdapter(TGGRule pRule) {
 	rule = pRule;
+	graphBuilder = new GraphBuilder();
+	/* Adds edges to the graph */
+	Collection<Edge> edges = rule.getEdges()//
+		.stream()//
+		.map(m -> new TGGRuleEdgeAdapter(m)).collect(Collectors.toList());
+	graphBuilder.addEdges(edges);
+	/* Adds nodes to the graph */
+	Collection<Node> nodes = rule.getNodes()//
+		.stream()//
+		.map(m -> new NodeAdapter(m)).collect(Collectors.toList());
+	nodes.stream().forEach((n) -> graphBuilder.addNode(n));
+
     }
 
     @Override
