caafac0a181a9110e32bc3145853190ba544dbc4
Jane Jose <jane.jose24@gmail.com>
2019-09-08 13:16:01 +0200

    Redesign of UserOptionsMenu (Issue #7)

--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/views/UserOptionsMenu.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/views/UserOptionsMenu.java
@@ -8,8 +8,8 @@ import org.eclipse.swt.events.ShellEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Scale;
 import org.eclipse.swt.widgets.Shell;
@@ -22,13 +22,23 @@ public class UserOptionsMenu {
     private MatchDisplayView matchDisplayView;
 
     private Shell menuShell;
-    private Button fullRuleVisButton;
+    private Button displayCreatedElementsButton;
     private Button displayCorrButton;
     private Button displaySrcButton;
     private Button displayTrgButton;
-    private Combo corrVisCombo;
-    private Combo edgeVisCombo;
-    private Combo nodeVisCombo;
+    
+    private Button edgeLabelCeckBox;
+    private Button edgeLabelAbbrRadioButton;
+    private Button edgeLabelFullRadioButton;
+    
+    private Button corrLabelCeckBox;
+    private Button corrLabelAbbrRadioButton;
+    private Button corrLabelFullRadioButton;
+    
+    private Button nodeLabelCeckBox;
+    private Button nodeLabelAbbrRadioButton;
+    private Button nodeLabelFullRadioButton;
+    
     private Scale neighborhoodScale;
 
     public UserOptionsMenu(UserOptionsManager pUserOptionsManager, MatchDisplayView pMatchDisplayView) {
@@ -51,20 +61,25 @@ public class UserOptionsMenu {
 	Composite panel = new Composite(menuShell, SWT.NONE);
 	panel.setLayoutData(new GridData(GridData.FILL_BOTH));
 	panel.setLayout(new GridLayout());
+	
+	Group graphElementsGroup = new Group(panel, SWT.NONE);
+	graphElementsGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+	graphElementsGroup.setLayout(new GridLayout());
+	graphElementsGroup.setText("Show/Hide parts of the graph");
 
-	fullRuleVisButton = new Button(panel, SWT.CHECK);
-	fullRuleVisButton.setText("Display created elements in match");
-	fullRuleVisButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-	fullRuleVisButton.addSelectionListener(new SelectionAdapter() {
+	displayCreatedElementsButton = new Button(graphElementsGroup, SWT.CHECK);
+	displayCreatedElementsButton.setText("Created elements");
+	displayCreatedElementsButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	displayCreatedElementsButton.addSelectionListener(new SelectionAdapter() {
 	    @Override
 	    public void widgetSelected(SelectionEvent pSelectionEvent) {
-		userOptionsManager.setDisplayFullRuleForMatches(fullRuleVisButton.getSelection());
+		userOptionsManager.setDisplayFullRuleForMatches(displayCreatedElementsButton.getSelection());
 		matchDisplayView.refresh();
 	    }
 	});
 
-	displaySrcButton = new Button(panel, SWT.CHECK);
-	displaySrcButton.setText("Display src elements in match");
+	displaySrcButton = new Button(graphElementsGroup, SWT.CHECK);
+	displaySrcButton.setText("SRC elements");
 	displaySrcButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
 	displaySrcButton.setSelection(userOptionsManager.displaySrcContextForMatches());
 	displaySrcButton.addSelectionListener(new SelectionAdapter() {
@@ -75,8 +90,8 @@ public class UserOptionsMenu {
 	    }
 	});
 
-	displayTrgButton = new Button(panel, SWT.CHECK);
-	displayTrgButton.setText("Display trg elements in match");
+	displayTrgButton = new Button(graphElementsGroup, SWT.CHECK);
+	displayTrgButton.setText("TRG elements");
 	displayTrgButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
 	displayTrgButton.setSelection(userOptionsManager.displayTrgContextForMatches());
 	displayTrgButton.addSelectionListener(new SelectionAdapter() {
@@ -87,8 +102,8 @@ public class UserOptionsMenu {
 	    }
 	});
 
-	displayCorrButton = new Button(panel, SWT.CHECK);
-	displayCorrButton.setText("Display corr elements in match");
+	displayCorrButton = new Button(graphElementsGroup, SWT.CHECK);
+	displayCorrButton.setText("CORR elements");
 	displayCorrButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
 	displayCorrButton.setSelection(userOptionsManager.displayCorrContextForMatches());
 	displayCorrButton.addSelectionListener(new SelectionAdapter() {
@@ -98,53 +113,207 @@ public class UserOptionsMenu {
 		matchDisplayView.refresh();
 	    }
 	});
-
-	edgeVisCombo = new Combo(panel, SWT.READ_ONLY);
-	edgeVisCombo.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-	for (String option : VisualizationLabelOptions.getOptionNames(VisualizationLabelOptions.EDGE))
-	    edgeVisCombo.add(option);
-	edgeVisCombo.select(userOptionsManager.getEdgeLabelVisualization().ordinal());
-	edgeVisCombo.addSelectionListener(new SelectionAdapter() {
+	
+	Group edgeLabelGroup = new Group(panel, SWT.NONE);
+	edgeLabelGroup.setLayout(new GridLayout());
+	edgeLabelGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+	edgeLabelGroup.setText("Edge labels");
+	
+	edgeLabelCeckBox = new Button(edgeLabelGroup, SWT.CHECK);
+	edgeLabelCeckBox.setText("Display edge labels");
+	edgeLabelCeckBox.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	edgeLabelCeckBox.setSelection(userOptionsManager.getEdgeLabelVisualization() != VisualizationLabelOptions.NONE);
+	edgeLabelCeckBox.addSelectionListener(new SelectionAdapter() {
 	    @Override
-	    public void widgetSelected(SelectionEvent e) {
-		userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.get(edgeVisCombo.getText()));
-		matchDisplayView.refresh();
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(!edgeLabelCeckBox.getSelection()) {
+	    		userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.NONE);
+	    		//disable radio buttons
+	    		edgeLabelAbbrRadioButton.setEnabled(false);
+	    		edgeLabelFullRadioButton.setEnabled(false);
+	    		matchDisplayView.refresh();
+	    	} else {
+	    		//set to radio option
+	    		edgeLabelAbbrRadioButton.setEnabled(true);
+	    		edgeLabelFullRadioButton.setEnabled(true);
+	    		if(edgeLabelAbbrRadioButton.getSelection()) {
+	    			userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		} else if(edgeLabelFullRadioButton.getSelection()) {
+	    			userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		}
+	    		matchDisplayView.refresh();
+	    	}
 	    }
 	});
-
-	nodeVisCombo = new Combo(panel, SWT.READ_ONLY);
-	nodeVisCombo.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-	for (String option : VisualizationLabelOptions.getOptionNames(VisualizationLabelOptions.NODE))
-	    nodeVisCombo.add(option);
-	nodeVisCombo.select(userOptionsManager.getNodeLabelVisualization().ordinal());
-	nodeVisCombo.addSelectionListener(new SelectionAdapter() {
+	
+	Composite edgeLabelComposite = new Composite(edgeLabelGroup, SWT.NONE);
+	edgeLabelComposite.setLayout(new GridLayout(2, false));
+	edgeLabelComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+	
+	edgeLabelAbbrRadioButton = new Button(edgeLabelComposite, SWT.RADIO);
+	edgeLabelAbbrRadioButton.setText("Abbreviated");
+	edgeLabelAbbrRadioButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+	edgeLabelAbbrRadioButton.setSelection(userOptionsManager.getEdgeLabelVisualization() == VisualizationLabelOptions.ABBREVIATED);
+	edgeLabelAbbrRadioButton.addSelectionListener(new SelectionAdapter() {
 	    @Override
-	    public void widgetSelected(SelectionEvent e) {
-		userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.get(nodeVisCombo.getText()));
-		matchDisplayView.refresh();
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(edgeLabelAbbrRadioButton.getSelection()) {
+	    		userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		matchDisplayView.refresh();
+	    	}
 	    }
 	});
-
-	corrVisCombo = new Combo(panel, SWT.READ_ONLY);
-	corrVisCombo.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-	for (String option : VisualizationLabelOptions.getOptionNames(VisualizationLabelOptions.CORR))
-	    corrVisCombo.add(option);
-	corrVisCombo.select(userOptionsManager.getCorrLabelVisualization().ordinal());
-	corrVisCombo.addSelectionListener(new SelectionAdapter() {
+	
+	edgeLabelFullRadioButton = new Button(edgeLabelComposite, SWT.RADIO);
+	edgeLabelFullRadioButton.setText("Full");
+	edgeLabelFullRadioButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
+	edgeLabelFullRadioButton.setSelection(userOptionsManager.getEdgeLabelVisualization() == VisualizationLabelOptions.FULLNAME);
+	edgeLabelFullRadioButton.addSelectionListener(new SelectionAdapter() {
 	    @Override
-	    public void widgetSelected(SelectionEvent e) {
-		userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.get(corrVisCombo.getText()));
-		matchDisplayView.refresh();
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(edgeLabelFullRadioButton.getSelection()) {
+	    		userOptionsManager.setEdgeLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		matchDisplayView.refresh();
+	    	}
 	    }
 	});
-
-	Composite neighborhoodOption = new Composite(panel, SWT.NONE);
+	
+	Group corrLabelGroup = new Group(panel, SWT.NONE);
+	corrLabelGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+	corrLabelGroup.setLayout(new GridLayout());
+	corrLabelGroup.setText("Correspondence Edge Labels");
+	
+	corrLabelCeckBox = new Button(corrLabelGroup, SWT.CHECK);
+	corrLabelCeckBox.setText("Display corr labels");
+	corrLabelCeckBox.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	corrLabelCeckBox.setSelection(userOptionsManager.getCorrLabelVisualization() != VisualizationLabelOptions.NONE);
+	corrLabelCeckBox.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(!corrLabelCeckBox.getSelection()) {
+	    		userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.NONE);
+	    		//disable radio buttons
+	    		corrLabelAbbrRadioButton.setEnabled(false);
+	    		corrLabelFullRadioButton.setEnabled(false);
+	    		matchDisplayView.refresh();
+	    	} else {
+	    		//set to radio option
+	    		corrLabelAbbrRadioButton.setEnabled(true);
+	    		corrLabelFullRadioButton.setEnabled(true);
+	    		if(corrLabelAbbrRadioButton.getSelection()) {
+	    			userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		} else if(corrLabelFullRadioButton.getSelection()) {
+	    			userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		}
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	Composite corrLabelComposite = new Composite(corrLabelGroup, SWT.NONE);
+	corrLabelComposite.setLayout(new GridLayout(2, false));
+	corrLabelComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+	
+	corrLabelAbbrRadioButton = new Button(corrLabelComposite, SWT.RADIO);
+	corrLabelAbbrRadioButton.setText("Abbreviated");
+	corrLabelAbbrRadioButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	corrLabelAbbrRadioButton.setSelection(userOptionsManager.getCorrLabelVisualization() == VisualizationLabelOptions.ABBREVIATED);
+	corrLabelAbbrRadioButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(corrLabelAbbrRadioButton.getSelection()) {
+	    		userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	corrLabelFullRadioButton = new Button(corrLabelComposite, SWT.RADIO);
+	corrLabelFullRadioButton.setText("Full");
+	corrLabelFullRadioButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	corrLabelFullRadioButton.setSelection(userOptionsManager.getCorrLabelVisualization() == VisualizationLabelOptions.FULLNAME);
+	corrLabelFullRadioButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(corrLabelFullRadioButton.getSelection()) {
+	    		userOptionsManager.setCorrLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	Group nodeLabelGroup = new Group(panel, SWT.NONE);
+	nodeLabelGroup.setLayoutData(new GridData(GridData.FILL_BOTH));
+	nodeLabelGroup.setLayout(new GridLayout());
+	nodeLabelGroup.setText("Node Labels");
+	
+	nodeLabelCeckBox = new Button(nodeLabelGroup, SWT.CHECK);
+	nodeLabelCeckBox.setText("Display node labels");
+	nodeLabelCeckBox.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	nodeLabelCeckBox.setSelection(userOptionsManager.getNodeLabelVisualization() != VisualizationLabelOptions.NONE);
+	nodeLabelCeckBox.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(!nodeLabelCeckBox.getSelection()) {
+	    		userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.NONE);
+	    		//disable radio buttons
+	    		nodeLabelAbbrRadioButton.setEnabled(false);
+	    		nodeLabelFullRadioButton.setEnabled(false);
+	    		matchDisplayView.refresh();
+	    	} else {
+	    		//set to radio option
+	    		nodeLabelAbbrRadioButton.setEnabled(true);
+	    		nodeLabelFullRadioButton.setEnabled(true);
+	    		if(nodeLabelAbbrRadioButton.getSelection()) {
+	    			userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		} else if(nodeLabelFullRadioButton.getSelection()) {
+	    			userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		}
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	Composite nodeLabelComposite = new Composite(nodeLabelGroup, SWT.NONE);
+	nodeLabelComposite.setLayout(new GridLayout(2, false));
+	nodeLabelComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+	
+	nodeLabelAbbrRadioButton = new Button(nodeLabelComposite, SWT.RADIO);
+	nodeLabelAbbrRadioButton.setText("Abbreviated");
+	nodeLabelAbbrRadioButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	nodeLabelAbbrRadioButton.setSelection(userOptionsManager.getNodeLabelVisualization() == VisualizationLabelOptions.ABBREVIATED);
+	nodeLabelAbbrRadioButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(nodeLabelAbbrRadioButton.getSelection()) {
+	    		userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.ABBREVIATED);
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	nodeLabelFullRadioButton = new Button(nodeLabelComposite, SWT.RADIO);
+	nodeLabelFullRadioButton.setText("Full");
+	nodeLabelFullRadioButton.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	nodeLabelFullRadioButton.setSelection(userOptionsManager.getNodeLabelVisualization() == VisualizationLabelOptions.FULLNAME);
+	nodeLabelFullRadioButton.addSelectionListener(new SelectionAdapter() {
+	    @Override
+	    public void widgetSelected(SelectionEvent pSelectionEvent) {
+	    	if(nodeLabelFullRadioButton.getSelection()) {
+	    		userOptionsManager.setNodeLabelVisualization(VisualizationLabelOptions.FULLNAME);
+	    		matchDisplayView.refresh();
+	    	}
+	    }
+	});
+	
+	Group neighborhoodOption = new Group(panel, SWT.NONE);
 	neighborhoodOption.setLayout(new GridLayout(2, false));
 	neighborhoodOption.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
+	neighborhoodOption.setText("Graph neighborhood");
 
 	Label neighborhoodLabel = new Label(neighborhoodOption, SWT.NONE);
 	neighborhoodLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
-	neighborhoodLabel.setText("Neighborhood size:");
+	neighborhoodLabel.setText("Size:");
 
 	neighborhoodScale = new Scale(neighborhoodOption, SWT.HORIZONTAL);
 	neighborhoodScale.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false));
