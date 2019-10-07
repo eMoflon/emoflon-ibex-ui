a9b86a10910664c2b7295d1432134417dfb04656
saman <sam.soltani@gmail.com>
2019-10-07 23:30:38 +0200

    adapting new approach for showing protocols

--- org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/EObjectAdapter.java
+++ org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/EObjectAdapter.java
@@ -5,10 +5,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.emoflon.ibex.tgg.ui.debug.api.Edge;
 import org.emoflon.ibex.tgg.ui.debug.api.Node;
 import org.emoflon.ibex.tgg.ui.debug.api.enums.Action;
 import org.emoflon.ibex.tgg.ui.debug.api.enums.Domain;
@@ -23,9 +25,9 @@ public class EObjectAdapter implements Node {
 		return wrappers.get(eObject);
 	}
 
-	public static EObjectAdapter adapt(EObject eObject, Domain domain) {
+	public static EObjectAdapter adapt(EObject eObject, Domain domain, Action action) {
 		if (!wrappers.containsKey(eObject))
-			wrappers.put(eObject, new EObjectAdapter(eObject, domain));
+			wrappers.put(eObject, new EObjectAdapter(eObject, domain, action));
 		return wrappers.get(eObject);
 	}
 
@@ -36,11 +38,13 @@ public class EObjectAdapter implements Node {
 	private String label;
 	private String index;
 	private Domain domain;
+	private Action action;
 	private List<String> attributes;
 
-	private EObjectAdapter(EObject eObject, Domain domain) {
+	private EObjectAdapter(EObject eObject, Domain domain, Action action) {
 		object = eObject;
 		this.domain = domain;
+		this.action = action;
 
 		label = eObject.eContainingFeature() != null ? object.eContainingFeature().getName() : "root";
 
@@ -50,7 +54,7 @@ public class EObjectAdapter implements Node {
 					+ resource.getContents().indexOf(eObject);
 		} else {
 			EObject container = eObject.eContainer();
-			index = EObjectAdapter.adapt(container, domain).getIndex() + "_" + container.eContents().indexOf(eObject);
+			index = EObjectAdapter.adapt(container, domain, Action.CONTEXT).getIndex() + "_" + container.eContents().indexOf(eObject);
 		}
 
 		attributes = new ArrayList<>();
@@ -79,7 +83,7 @@ public class EObjectAdapter implements Node {
 
 	@Override
 	public Action getAction() {
-		return Action.CONTEXT;
+		return action;
 	}
 
 	@Override
@@ -87,29 +91,132 @@ public class EObjectAdapter implements Node {
 		return attributes;
 	}
 
+	public void setAction(Action pAction) {
+		action = pAction;
+	}
+
 	public static void constructGraphDomain(GraphBuilder builder, Domain domain, Collection<EObject> domainObjects) {
 		for (EObject object : domainObjects) {
-			builder.addNode(EObjectAdapter.adapt(object, domain));
+			builder.addNode(EObjectAdapter.adapt(object, domain, Action.CONTEXT));
 			for (EObject contentObject : object.eContents()) {
 				builder.addEdge(contentObject.eContainingFeature().getName(), EObjectAdapter.get(object),
-						EObjectAdapter.adapt(contentObject, domain), EdgeType.NORMAL, Action.CONTEXT);
+						EObjectAdapter.adapt(contentObject, domain, Action.CONTEXT), EdgeType.NORMAL, Action.CONTEXT);
 			}
 			for (EObject crossReference : object.eCrossReferences()) {
 				builder.addEdge(crossReference.eContainingFeature().getName(), EObjectAdapter.get(object),
-						EObjectAdapter.adapt(crossReference, domain), EdgeType.NORMAL, Action.CONTEXT);
+						EObjectAdapter.adapt(crossReference, domain, Action.CONTEXT), EdgeType.NORMAL, Action.CONTEXT);
 			}
 		}
 	}
 
+	public static void constructGraphDomain(GraphBuilder builder, Domain domain, Collection<EObject> domainObjects, TGGRuleAdapter rule) {
+		for (EObject object : domainObjects) {
+			EObjectAdapter src = EObjectAdapter.adapt(object, domain, Action.CONTEXT);
+			Optional<TGGRuleNodeAdapter> srcn = rule.getNodes().stream().filter(n -> n.getType().equals(src.getType()) && n.getDomain().equals(src.getDomain())).findFirst();
+			if (srcn.isPresent()) {
+				src.setAction(srcn.get().getAction());
+			}
+			builder.addNode(src);
+			for (EObject contentObject : object.eContents()) {
+				EObjectAdapter trg = EObjectAdapter.adapt(contentObject, domain, Action.CONTEXT);
+				Optional<TGGRuleNodeAdapter> trgn = rule.getNodes().stream().filter(n -> n.getType().equals(trg.getType()) && n.getDomain().equals(trg.getDomain())).findFirst();
+
+				Action action = Action.CONTEXT;
+				EdgeType edgeType = EdgeType.NORMAL;
+
+				if (trgn.isPresent()) {
+					trg.setAction(trgn.get().getAction());
+					
+					if (srcn.isPresent()) {
+						Optional<Edge> edge = rule.getGraph().getEdges().stream().filter(e ->
+							!e.getType().equals(EdgeType.CORR) &&
+							e.getSrcNode().equals(srcn.get()) &&
+							e.getTrgNode().equals(trgn.get()))
+						.findFirst();
+						
+						if (edge.isPresent()) {
+							action = edge.get().getAction(); 
+							edgeType = edge.get().getType();
+						}
+					}
+				}
+				 
+				builder.addEdge(contentObject.eContainingFeature().getName(), src, trg, edgeType, action);
+			}
+			for (EObject crossReference : object.eCrossReferences()) {
+				EObjectAdapter trg = EObjectAdapter.adapt(crossReference, domain, Action.CONTEXT);
+				Optional<TGGRuleNodeAdapter> trgn = rule.getNodes().stream().filter(n -> n.getType().equals(trg.getType()) && n.getDomain().equals(trg.getDomain())).findFirst();
+
+				Action action = Action.CONTEXT;
+				EdgeType edgeType = EdgeType.NORMAL;
+
+				if (trgn.isPresent()) {
+					trg.setAction(trgn.get().getAction());
+					
+					if (srcn.isPresent()) {
+						Optional<Edge> edge = rule.getGraph().getEdges().stream().filter(e ->
+							!e.getType().equals(EdgeType.CORR) &&
+							e.getSrcNode().equals(srcn.get()) &&
+							e.getTrgNode().equals(trgn.get()))
+						.findFirst();
+						
+						if (edge.isPresent()) {
+							action = edge.get().getAction(); 
+							edgeType = edge.get().getType();
+						}
+					}
+				}
+				
+				builder.addEdge(crossReference.eContainingFeature().getName(), src, trg, edgeType, action);
+			}
+		}
+	}	
+	
 	public static void constructCorrEdges(GraphBuilder builder, Collection<EObject> corrObjects) {
 		for (EObject corrObject : corrObjects) {
 			builder.addEdge(":" + corrObject.eClass().getName(), //
 					EObjectAdapter.adapt((EObject) corrObject.eGet(corrObject.eClass().getEStructuralFeature("source")),
-							Domain.SRC), //
+							Domain.SRC, Action.CONTEXT), //
 					EObjectAdapter.adapt((EObject) corrObject.eGet(corrObject.eClass().getEStructuralFeature("target")),
-							Domain.TRG), //
+							Domain.TRG, Action.CONTEXT), //
 					EdgeType.CORR, //
 					Action.CONTEXT);
 		}
 	}
+	
+	public static void constructCorrEdges(GraphBuilder builder, Collection<EObject> corrObjects, TGGRuleAdapter rule) {
+		for (EObject corrObject : corrObjects) {
+			Action action = Action.CONTEXT;
+			EdgeType edgeType = EdgeType.NORMAL;
+			
+			EObjectAdapter src = EObjectAdapter.adapt((EObject) corrObject.eGet(corrObject.eClass().getEStructuralFeature("source")), Domain.SRC, Action.CONTEXT);
+			EObjectAdapter trg = EObjectAdapter.adapt((EObject) corrObject.eGet(corrObject.eClass().getEStructuralFeature("target")),
+					Domain.TRG, Action.CONTEXT);
+			
+			Optional<TGGRuleNodeAdapter> srcn = rule.getNodes().stream().filter(n -> n.getType().equals(src.getType()) && n.getDomain().equals(src.getDomain())).findFirst();
+			if (srcn.isPresent()) {
+				src.setAction(srcn.get().getAction());
+			}
+			
+			Optional<TGGRuleNodeAdapter> trgn = rule.getNodes().stream().filter(n -> n.getType().equals(trg.getType()) && n.getDomain().equals(trg.getDomain())).findFirst();
+			if (trgn.isPresent()) {
+				trg.setAction(trgn.get().getAction());
+			}
+			
+			if (srcn.isPresent() && trgn.isPresent()) {
+				Optional<Edge> edge = rule.getGraph().getEdges().stream().filter(e ->
+					e.getType().equals(EdgeType.CORR) &&
+					e.getSrcNode().equals(srcn.get()) &&
+					e.getTrgNode().equals(trgn.get()))
+				.findFirst();
+			
+				if (edge.isPresent()) {
+					action = edge.get().getAction(); 
+					edgeType = edge.get().getType();
+				}
+			}
+			
+			builder.addEdge(":" + corrObject.eClass().getName(), src, trg, edgeType, action);
+		}
+	}	
 }
--- org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/ProtocolStepAdapter.java
+++ org.emoflon.ibex.tgg.ui.debug.adapter/src/org/emoflon/ibex/tgg/ui/debug/adapter/TGGAdpater/ProtocolStepAdapter.java
@@ -2,14 +2,18 @@ package org.emoflon.ibex.tgg.ui.debug.adapter.TGGAdpater;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
-
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.emoflon.ibex.tgg.operational.monitoring.data.ProtocolStep;
 import org.emoflon.ibex.tgg.operational.monitoring.data.TGGObjectGraph;
 import org.emoflon.ibex.tgg.operational.monitoring.data.TGGObjectGraphBuilder;
 import org.emoflon.ibex.tgg.ui.debug.api.Graph;
+import org.emoflon.ibex.tgg.ui.debug.api.Node;
 import org.emoflon.ibex.tgg.ui.debug.api.RuleApplication;
 import org.emoflon.ibex.tgg.ui.debug.api.RuleApplicationMerger;
+import org.emoflon.ibex.tgg.ui.debug.api.enums.Action;
 import org.emoflon.ibex.tgg.ui.debug.api.enums.Domain;
 import org.emoflon.ibex.tgg.ui.debug.api.impl.GraphBuilder;
 
@@ -44,24 +48,39 @@ public class ProtocolStepAdapter implements RuleApplication {
 	private static class ProtocolStepMerger implements RuleApplicationMerger {
 		@Override
 		public Graph getMergedGraph(Collection<RuleApplication> ruleApplications, int neighbourhoodSize) {
-
+			
 			TGGObjectGraphBuilder objectGraphBuilder = new TGGObjectGraphBuilder();
+			TGGObjectGraphBuilder tObjectGraphBuilder = new TGGObjectGraphBuilder();
+			TGGRuleAdapter rule = null;
 			for (RuleApplication ruleApplication : ruleApplications) {
 				if (!(ruleApplication instanceof ProtocolStepAdapter))
 					throw new IllegalStateException(
 							"This merger only supports RuleApplications of type ProtocolStepAdapter.");
+				
+				ProtocolStep cps = ((ProtocolStepAdapter) ruleApplication).protocolStep;
+				rule = TGGRuleAdapter.getRuleByName(cps.getAppliedRule().getName());
 
-				objectGraphBuilder.add(((ProtocolStepAdapter) ruleApplication).protocolStep.getObjectGraph());
+				wrappers.keySet().stream().filter(p -> p.getIndex() == cps.getIndex()).forEach(p -> tObjectGraphBuilder.add(p.getObjectGraph()));
+				
+				TGGObjectGraph tGraph = tObjectGraphBuilder.build();
+//				tGraph = constructNewGraph(rule, tGraph);
+				
+				objectGraphBuilder.add(tGraph);
+				
 			}
+			
 			TGGObjectGraph objectGraph = objectGraphBuilder.build();
 
 			GraphBuilder builder = new GraphBuilder();
+			
 			EObjectAdapter.constructGraphDomain(builder, Domain.SRC,
-					VictoryIBeXAdapter.getNeighbourhood(objectGraph.getSrcElements(), neighbourhoodSize));
+					VictoryIBeXAdapter.getNeighbourhood(objectGraph.getSrcElements(), neighbourhoodSize), rule);
 			EObjectAdapter.constructGraphDomain(builder, Domain.TRG,
-					VictoryIBeXAdapter.getNeighbourhood(objectGraph.getTrgElements(), neighbourhoodSize));
-			EObjectAdapter.constructCorrEdges(builder, objectGraph.getCorrElements());
+					VictoryIBeXAdapter.getNeighbourhood(objectGraph.getTrgElements(), neighbourhoodSize), rule);
+			EObjectAdapter.constructCorrEdges(builder, objectGraph.getCorrElements(), rule);
+			
 			return builder.build();
+
 		}
 	}
 
@@ -69,4 +88,49 @@ public class ProtocolStepAdapter implements RuleApplication {
 	public String getRuleName() {
 		return protocolStep.getAppliedRule().getName();
 	}
+	
+	private static TGGObjectGraph constructNewGraph(TGGRuleAdapter rule, TGGObjectGraph objectGraph) {	
+		
+		Collection<EObject> src = new HashSet<>();
+		Collection<EObject> trg = new HashSet<>();		
+		
+		for (EObject obj : objectGraph.getSrcElements()) {
+			EObjectAdapter node = EObjectAdapter.adapt(obj, Domain.SRC, Action.CONTEXT);
+			Collection <Node> nodes = new HashSet<Node>(); 
+			rule.getGraph().getEdges().stream().filter(e -> e.getAction().equals(Action.CREATE)).forEach(e -> {
+				nodes.add(e.getSrcNode());
+				nodes.add(e.getTrgNode());
+			});
+			if (nodes.stream().anyMatch(n ->
+					n.getType().equals(node.getType()) &&
+					n.getDomain().equals(node.getDomain())
+					)) {
+				src.add(obj);
+			} else {
+				System.out.println(node.getType());
+				EcoreUtil.remove(obj);
+			}
+		} 
+		
+		for (EObject obj : objectGraph.getTrgElements()) {
+			EObjectAdapter node = EObjectAdapter.adapt(obj, Domain.TRG, Action.CONTEXT);
+			Collection <Node> nodes = new HashSet<Node>(); 
+			rule.getGraph().getEdges().stream().filter(e -> e.getAction().equals(Action.CREATE)).forEach(e -> {
+				nodes.add(e.getSrcNode());
+				nodes.add(e.getTrgNode());
+			});
+			if (nodes.stream().anyMatch(n ->
+					n.getType().equals(node.getType()) &&
+					n.getDomain().equals(node.getDomain())
+					)) {
+				trg.add(obj);
+			} else {
+				System.out.println(node.getType());
+				EcoreUtil.remove(obj);
+			}
+		} 		
+		
+		return new TGGObjectGraph(src, trg, objectGraph.getCorrElements());
+	}
+	
 }
