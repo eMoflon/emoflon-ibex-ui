4d660691dfad6526e237ff8987aecfb030d607e2
Mario Treiber <mariotreiber@gmail.com>
2019-10-07 15:12:54 +0200

    Added Javadocs for the graph building methods of Match and RuleApplication

--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Match.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/Match.java
@@ -13,5 +13,18 @@ public interface Match {
 
 	public Rule getRule();
 
+	/**
+	 * The returned graph should explicitly contain:<br>
+	 * 1) all model-nodes that are part of the match<br>
+	 * 2) all edges between those model-nodes<br>
+	 * 3) edges from each rule-node to its matched model-node
+	 * <p>
+	 * The rule-nodes in 3) should NOT be included in the nodes of this graph. They
+	 * should only be referenced by the edges.
+	 * 
+	 * @param neighbourhoodSize
+	 *            the neighbourhood size setting for which the graph should be built
+	 * @return a graph for this match
+	 */
 	public Graph getGraph(int neighbourhoodSize);
 }
--- org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/RuleApplicationMerger.java
+++ org.emoflon.ibex.tgg.ui.debug/src/org/emoflon/ibex/tgg/ui/debug/api/RuleApplicationMerger.java
@@ -7,5 +7,16 @@ import java.util.Collection;
  * single Graph for joint visualization.
  */
 public interface RuleApplicationMerger {
+	/**
+	 * Builds a graph for the specified collection of rule-applications.<br>
+	 * Which elements of the rule-applications are included is up to the
+	 * implementation to decide.
+	 * 
+	 * @param ruleApplications
+	 *            the rule-applications to build the graph for
+	 * @param neighbourhoodSize
+	 *            the neighbourhood size setting made by the user
+	 * @return a graph for the specified collection of rule-applications
+	 */
 	public Graph getMergedGraph(Collection<RuleApplication> ruleApplications, int neighbourhoodSize);
 }
