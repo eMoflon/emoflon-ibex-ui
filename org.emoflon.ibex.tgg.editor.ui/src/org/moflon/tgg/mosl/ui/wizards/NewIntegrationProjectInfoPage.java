b62d8aaa49fe85dab0cf6515123bec92cb76cf18
Adrian Möller <45700082+adrianmoeller@users.noreply.github.com>
2022-06-23 13:45:02 +0200

    Delete old xtext project

--- org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationProjectInfoPage.java
+++
@@ -1,9 +0,0 @@
-package org.moflon.tgg.mosl.ui.wizards;
-
-public class NewIntegrationProjectInfoPage extends AbstractMoflonProjectInfoPage {
-
-	public NewIntegrationProjectInfoPage() {
-		super("NewIntegrationProjectInfo", "New TGG Project", "This wizard creates a new eMoflon TGG Project");
-	}
-
-}
--- org.emoflon.ibex.tgg.editor.ui/src/org/moflon/tgg/mosl/ui/wizards/NewIntegrationProjectTrgModelSelectionPage.java
+++
@@ -1,9 +0,0 @@
-package org.moflon.tgg.mosl.ui.wizards;
-
-public class NewIntegrationProjectTrgModelSelectionPage extends AbstractMoflonMetaModelSelectionPage {
-
-	public NewIntegrationProjectTrgModelSelectionPage() {
-		super("NewIntegrationProjectTrgModelSelection", "New TGG Project", "Selection of target metamodels", "Select target metamodels:");
-	}
-
-}
