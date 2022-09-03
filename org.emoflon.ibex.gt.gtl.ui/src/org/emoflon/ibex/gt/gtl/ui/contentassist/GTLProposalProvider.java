81bb1942856ec52158d7828634feeb9c2dc1a015
SebastianE <sebastian.ehmes@gmail.com>
2022-09-03 16:15:13 +0200

    updated jre config to java 17 + added gt file import proposal

--- org.emoflon.ibex.gt.gtl.ui/src/org/emoflon/ibex/gt/gtl/ui/contentassist/GTLProposalProvider.java
+++ org.emoflon.ibex.gt.gtl.ui/src/org/emoflon/ibex/gt/gtl/ui/contentassist/GTLProposalProvider.java
@@ -4,14 +4,29 @@
 package org.emoflon.ibex.gt.gtl.ui.contentassist;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.text.contentassist.CompletionProposal;
+import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.ui.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ui.editor.contentassist.ICompletionProposalAcceptor;
 import org.emoflon.ibex.common.slimgt.util.SlimGTWorkspaceUtils;
+import org.emoflon.ibex.gt.gtl.gTL.EditorFile;
 
 /**
  * See
@@ -81,6 +96,94 @@ public class GTLProposalProvider extends AbstractGTLProposalProvider {
 				: context.getCurrentNode().getText().length() - currentSelection.length();
 
 		acceptor.accept(new CompletionProposal(pkgName.toLowerCase(), context.getOffset(), replacementLength, cursor));
+	}
+
+	@Override
+	public void completePatternImport_File(EObject model, Assignment assignment, ContentAssistContext context,
+			ICompletionProposalAcceptor acceptor) {
+		super.completePatternImport_File(model, assignment, context, acceptor);
+		IProject currentProject = SlimGTWorkspaceUtils.getCurrentProject(model.eResource());
+		String currentFile = model.eResource().getURI().toString().replace("platform:/resource/", "")
+				.replace(currentProject.getName(), "");
+		currentFile = currentProject.getLocation().toPortableString() + currentFile;
+		currentFile = currentFile.replace("/", "\\");
+
+		String[] lines = context.getCurrentNode().getText().split("\n");
+		lines = lines[0].split("\r");
+
+		String currentSelection = lines[0].replace("\"", "").replace("/", "\\").trim().replace("%20", " ");
+		String rest = context.getCurrentNode().getText().replace(lines[0], "");
+
+		Map<String, EObject> path2model = new HashMap<>();
+		IWorkspace ws = ResourcesPlugin.getWorkspace();
+		for (IProject project : ws.getRoot().getProjects()) {
+
+			try {
+				if (!project.hasNature("org.emoflon.ibex.gt.gtl.ui.nature"))
+					continue;
+			} catch (CoreException e) {
+				continue;
+			}
+
+			File projectFile = new File(project.getLocation().toPortableString());
+			List<File> gtlFiles = new LinkedList<>();
+			SlimGTWorkspaceUtils.gatherFilesWithEnding(gtlFiles, projectFile, ".gtl", true);
+
+			for (File gtFile : gtlFiles) {
+
+				XtextResourceSet rs = new XtextResourceSet();
+				URI gtModelUri;
+				try {
+					gtModelUri = URI.createFileURI(gtFile.getCanonicalPath());
+				} catch (IOException e) {
+					continue;
+				}
+
+				String fileString = gtModelUri.toFileString();
+				if (fileString.equals(currentFile))
+					continue;
+
+				if (!currentSelection.isBlank() && !fileString.contains(currentSelection))
+					continue;
+
+				Resource resource = rs.getResource(gtModelUri, true);
+				EcoreUtil2.resolveLazyCrossReferences(resource, () -> false);
+				EObject gtModel = resource.getContents().get(0);
+
+				if (gtModel == null)
+					continue;
+
+				// Put absolute path
+				path2model.put(gtModelUri.toFileString(), gtModel);
+
+				// Put project relative path
+				String relativePath = Paths.get(currentProject.getLocation().toPortableString())
+						.relativize(Paths.get(gtModelUri.toFileString())).toString();
+				path2model.put(relativePath, gtModel);
+
+				// Put package as an alternative to path
+				if (gtModel instanceof EditorFile gipsEditorFile) {
+					path2model.put(gipsEditorFile.getPackage().getName().replace("\"", ""), gtModel);
+				}
+
+			}
+		}
+
+		for (String path : path2model.keySet()) {
+
+			String replacement = "\"" + path + "\"";
+			int start = (currentSelection.isBlank()) ? 0 : currentSelection.length() + 1;
+			replacement = replacement.substring(start);
+			int cursor = replacement.length();
+			replacement = replacement + rest;
+
+			int replacementLength = (currentSelection.isBlank())
+					? context.getCurrentNode().getText().length() - currentSelection.length() - 1
+					: context.getCurrentNode().getText().length() - currentSelection.length();
+
+			acceptor.accept(new CompletionProposal(replacement, context.getOffset(), replacementLength, cursor));
+		}
+
 	}
 
 }
