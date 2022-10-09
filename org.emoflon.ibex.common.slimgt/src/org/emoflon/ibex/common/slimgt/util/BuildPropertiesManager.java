c1899d7715658b9b513239c5c6664a00208c8110
SebastianE <sebastian.ehmes@gmail.com>
2022-10-09 20:30:37 +0200

    finished BuildPropertiesManager

--- org.emoflon.ibex.common.slimgt/src/org/emoflon/ibex/common/slimgt/util/BuildPropertiesManager.java
+++ org.emoflon.ibex.common.slimgt/src/org/emoflon/ibex/common/slimgt/util/BuildPropertiesManager.java
@@ -3,18 +3,23 @@ package org.emoflon.ibex.common.slimgt.util;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
+
+import com.google.common.base.Strings;
 
 public class BuildPropertiesManager {
 
 	final protected IProject project;
 	protected File bpFile;
 	protected Map<String, Set<String>> attribute2values = new LinkedHashMap<>();
+	protected boolean dirty = false;
 
 	public BuildPropertiesManager(final IProject project) throws IOException {
 		this.project = project;
@@ -28,7 +33,79 @@ public class BuildPropertiesManager {
 		return bpFile.exists();
 	}
 
-//	public boo
+	public boolean hasAttribute(final String attribute) {
+		return attribute2values.containsKey(attribute);
+	}
+
+	public boolean hasValue(final String attribute, final String value) {
+		Set<String> values = attribute2values.get(attribute);
+		if (values == null) {
+			return false;
+		}
+
+		return values.contains(value);
+	}
+
+	public void addAttributeValue(final String attribute, final String value) {
+		Set<String> values = attribute2values.get(attribute);
+		if (values == null) {
+			values = new LinkedHashSet<>();
+			attribute2values.put(attribute, values);
+		}
+
+		if (!values.contains(value)) {
+			values.add(value);
+			dirty = true;
+		}
+	}
+
+	public void setAttributeValues(final String attribute, final Collection<String> values) {
+		Set<String> otherValues = attribute2values.get(attribute);
+		if (values == null) {
+			otherValues = new LinkedHashSet<>(values);
+			attribute2values.put(attribute, otherValues);
+			dirty = true;
+			return;
+		}
+
+		for (String value : values) {
+			if (!otherValues.contains(value)) {
+				otherValues.add(value);
+			} else {
+				continue;
+			}
+		}
+	}
+
+	public void writeBack() {
+		if (!dirty)
+			return;
+
+		StringBuilder sb = new StringBuilder();
+		for (String attribute : attribute2values.keySet()) {
+			String attributeDeclaration = attribute + " = ";
+			sb.append(attributeDeclaration);
+
+			LinkedList<String> values = new LinkedList<>(attribute2values.get(attribute));
+			sb.append(values.poll());
+			if (!values.isEmpty()) {
+				sb.append(",\\\n");
+			} else {
+				sb.append("\n");
+				return;
+			}
+
+			while (values.size() > 1) {
+				sb.append(Strings.repeat(" ", attributeDeclaration.length()));
+				sb.append(values.poll());
+				sb.append(",\\\n");
+			}
+
+			sb.append(Strings.repeat(" ", attributeDeclaration.length()));
+			sb.append(values.poll());
+			sb.append("\n");
+		}
+	}
 
 	protected void loadProperties() throws IOException {
 		String content = Files.readString(bpFile.toPath());
