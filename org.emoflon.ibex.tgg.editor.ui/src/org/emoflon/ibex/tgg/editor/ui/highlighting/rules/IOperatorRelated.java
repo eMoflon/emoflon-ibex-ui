c3886244c7d30b6fb81f6d78c4e0fe7b8833ddd0
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-10-12 15:31:53 +0200

    Removes old IBeX 1.0 projects

--- org.emoflon.ibex.tgg.editor.ui/src/org/emoflon/ibex/tgg/editor/ui/highlighting/rules/IOperatorRelated.java
+++
@@ -1,7 +0,0 @@
-package org.emoflon.ibex.tgg.editor.ui.highlighting.rules;
-
-import org.emoflon.ibex.tgg.editor.tgg.Operator;
-
-public interface IOperatorRelated {
-	public boolean getOperatorCondition(Operator op);
-}
