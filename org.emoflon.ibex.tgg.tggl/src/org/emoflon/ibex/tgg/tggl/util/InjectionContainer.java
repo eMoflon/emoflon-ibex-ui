ba952bdb035642fe04baa5e6a77150c4e083aadb
SebastianE <sebastian.ehmes@gmail.com>
2025-02-04 14:43:33 +0100

    + removed all TGG related components and dependencies

--- org.emoflon.ibex.tgg.tggl/src/org/emoflon/ibex/tgg/tggl/util/InjectionContainer.java
+++
@@ -1,8 +0,0 @@
-package org.emoflon.ibex.tgg.tggl.util;
-
-import org.eclipse.xtext.resource.IContainer;
-import org.eclipse.xtext.resource.impl.ResourceDescriptionsProvider;
-
-public record InjectionContainer(ResourceDescriptionsProvider resourceDescriptionsProvider, IContainer.Manager containerManager){
-
-}
--- org.emoflon.ibex.tgg.wizards/src/org/emoflon/ibex/tgg/editor/ui/wizards/NewIntegrationProjectInfoPage.java
+++
@@ -1,9 +0,0 @@
-package org.emoflon.ibex.tgg.editor.ui.wizards;
-
-public class NewIntegrationProjectInfoPage extends AbstractMoflonProjectInfoPage {
-
-	public NewIntegrationProjectInfoPage() {
-		super("NewIntegrationProjectInfo", "New TGG Project", "This wizard creates a new eMoflon TGG Project");
-	}
-
-}
